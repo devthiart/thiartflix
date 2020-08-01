ebec79d807d2a66f0f1fb2f1e181cd07c0d90f9c
devthiart <devthiart@gmail.com>
2020-08-01 11:05:41 -0300

    Aula 2 Inclusão de rotas e outras páginas

--- src/Components/PageDefault/index.js
+++ src/Components/PageDefault/index.js
@@ -4,8 +4,6 @@ import Footer from '../Footer';
 import styled from 'styled-components';
 
 const Main = styled.main`
-    background-color: var(--blackBackground);
-    color: var(--white);
     flex: 1;
     padding-top: 50px;
     padding-left: 5%;
